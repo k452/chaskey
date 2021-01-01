572ec53c08d64053fd3cbd0e2ae2dfc592b6d9c8
k452 <55301839+k452@users.noreply.github.com>
2021-01-01 23:46:28 +0900

    ランダムな平文と鍵の生成

--- main.go
+++ main.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
+	"time"
 )
 
 const blockLen = 32
 const keyLen = 32
 const splitLen = 8
+const times = 3
 
 func main() {
-	plainText := []int{0b10000000, 0b00000000, 0b00000000, 0b00000000}
-	k := 0b00000000100000001000000010000000
-	k1 := createK1(k)
-	fmt.Printf("K:   %032b\n", k)
-	fmt.Printf("K1:  %032b\n", k1)
-	fmt.Printf("平文: %08b\n", plainText)
-	res := plainText
-	for i := 0; i < 3; i++ {
-		fmt.Printf("π関数%d段目\n", i+1)
-		res = permutation(res)
+	texts := random(0b0, 0b11111111111111111111111111111111, times)
+	//plainText := []int{0b10000000, 0b00000000, 0b00000000, 0b00000000}
+	keys := random(0b0, 0b11111111111111111111111111111111, times)
+	//k := 0b00000000100000001000000010000000
+	//k1 := createK1(k)
+
+	for i := 0; i < times; i++ {
+		var text []int
+		text = append(text, texts[i]>>24&0xff)
+		text = append(text, texts[i]>>16&0xff)
+		text = append(text, texts[i]>>8&0xff)
+		text = append(text, texts[i]&0xff)
+
+		fmt.Printf("乱数:   %d個目\n", i)
+		fmt.Printf("平文: %08b\n", text)
+		fmt.Printf("K:   %032b\n", keys[i])
+		fmt.Printf("K1:  %032b\n", createK1(keys[i]))
+		res := text
+		for j := 0; j < 3; j++ {
+			fmt.Printf("π関数%d段目\n", j+1)
+			res = permutation(res)
+		}
+		fmt.Println("----------------------------------------------------------")
 	}
 }
 
@@ -31,27 +48,27 @@ func permutation(vIn []int) []int {
 	var vOut []int
 
 	//v3
-	v3_1 := RotateL(vIn[3], 2)
+	v3_1 := RotateL8(vIn[3], 2)
 	v3_2 := (modPlus(vIn[2], vIn[3]) ^ v3_1)
 
 	//v0
 	v0_1 := modPlus(vIn[0], vIn[1])
-	v0_2 := RotateL(v0_1, 4)
+	v0_2 := RotateL8(v0_1, 4)
 	v0_3 := modPlus(v3_2, v0_2)
 
 	//v1
-	v1_1 := RotateL(vIn[1], 3)
+	v1_1 := RotateL8(vIn[1], 3)
 	v1_2 := modPlus(vIn[1], vIn[0]) ^ v1_1
-	v1_3 := RotateL(v1_2, 3)
+	v1_3 := RotateL8(v1_2, 3)
 	v1_4 := modPlus(v1_2, vIn[2]) ^ v1_3
 
 	//v2
 	v2_1 := modPlus(vIn[2], vIn[3])
 	v2_2 := modPlus(v1_2, v2_1)
-	v2_3 := RotateL(v2_2, 4)
+	v2_3 := RotateL8(v2_2, 4)
 
 	//v3
-	v3_3 := RotateL(v3_2, 1)
+	v3_3 := RotateL8(v3_2, 1)
 	v3_4 := v0_3 ^ v3_3
 
 	vOut = append(vOut, v0_3)
@@ -72,7 +89,7 @@ func modPlus(a int, b int) int {
 	return (a + b) % 256
 }
 
-func RotateL(a int, i int) int {
+func RotateL8(a int, i int) int {
 	return ((a<<i)&0xff ^ (a >> (8 - i)))
 }
 
@@ -90,6 +107,34 @@ func createK1(k int) int {
 
 func createK2(k1 int) int {
 	return createK1(createK1(k1))
+}
+
+func k(m map[int]bool) []int {
+	i := 0
+	result := make([]int, len(m))
+	for key, _ := range m {
+		result[i] = key
+		i++
+	}
+	return result
+}
+
+func random(min int, max int, num int) []int {
+	numRange := max - min
+
+	selected := make(map[int]bool)
+	rand.Seed(time.Now().UnixNano())
+	for counter := 0; counter < num; {
+		n := rand.Intn(numRange) + min
+		if selected[n] == false {
+			selected[n] = true
+			counter++
+		}
+	}
+	keys := k(selected)
+	// ソートしたくない場合は以下1行をコメントアウト
+	sort.Sort(sort.IntSlice(keys))
+	return keys
 }
 
 /*
