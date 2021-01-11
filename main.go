40aaf75e0be884266399a522da81b08864f16219
k452 <55301839+k452@users.noreply.github.com>
2021-01-11 18:46:50 +0900

    1段の暗号(31階差分)

--- main.go
+++ main.go
@@ -2,67 +2,60 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
-	"sort"
 	"time"
 )
 
-const blockLen = 32
-const keyLen = 32
-const splitLen = 8
-const piRound = 2
-
-var times int = int(math.Pow(2, 3)) //最終的には(2, 32)にする
+const blockLen = 32 //ブロック長
+const keyLen = 32   //鍵長
+const splitLen = 8  //
+const piRound = 2   //π関数の中の転置の段数
+const times = 1     //試行回数
 
 func main() {
-	sabun := random(0b0, 0b00000000000000000000000000000111, int(math.Pow(2, 3)))
+	//実行時間の計測開始
+	start := time.Now()
+
+	//平文をランダム生成
 	texts := random(0b0, 0b11111111111111111111111111111111, times)
-	keys := random(0b0, 0b11111111111111111111111111111111, 1)
+	keys := random(0b0, 0b11111111111111111111111111111111, times)
 
+	//暗号化を複数回実行
 	for i := 0; i < times; i++ {
-		result := []int{
-			0b00000000,
-			0b00000000,
-			0b00000000,
-			0b00000000,
-		}
-		for _, v := range sabun {
-			var text []int
-			text = append(text, (texts[i]>>24&0xff)^v)
-			text = append(text, (texts[i]>>16&0xff)^v)
-			text = append(text, (texts[i]>>8&0xff)^v)
-			text = append(text, (texts[i]&0xff)^v)
-
-			//fmt.Printf("試行: %d回目\n", i+1)
-			//fmt.Printf("差分: %d個目\n", j+1)
-			//fmt.Printf("平文: %08b\n", text)
-			//fmt.Printf("K:    %032b\n", keys[i])
-			//fmt.Printf("K1:   %032b\n", createK1(keys[i]))
-
-			for k := 0; k < piRound; k++ {
-				text = permutation(text)
-			}
-			//fmt.Printf("出力: %08b\n", text)
-			for l := 0; l < 4; l++ {
-				result[l] = text[l] ^ result[l]
-			}
-			//fmt.Println("----------------------------------------------------------")
-		}
-		fmt.Printf("試行: %d回目\n", i+1)
-		fmt.Printf("結果: %08b\n", result)
-		fmt.Println("----------------------------------------------------------")
-		result = []int{
-			0b00000000,
-			0b00000000,
-			0b00000000,
-			0b00000000,
-		}
+		chaskey(texts[i], keys[i], i)
 	}
+
+	//実行時間の表示
+	fmt.Println(time.Since(start))
 }
 
-func permutation(vIn []int) []int {
-	var vOut []int
+//chaskeyの暗号本体
+func chaskey(in, k, num int) {
+	result := 0b00000000000000000000000000000000
+	k1 := createK1(k)
+
+	for j := 0b0; j <= 0b1111111111111111111111111111111; j++ {
+		m := in ^ j      //差分ベクトルと平文を排他
+		m = (k ^ m) ^ k1 //鍵と平文と副鍵を排他
+		for k := 0; k < piRound; k++ {
+			m = permutation(m) //π関数
+		}
+		m ^= k1     //π関数の出力と副鍵を排他
+		result ^= m //結果をこれまでの結果と排他
+	}
+	fmt.Printf("試行: %d回目\n", num+1)
+	fmt.Printf("結果: %032b\n", result)
+	fmt.Println("----------------------------------------------------------")
+	result = 0b00000000000000000000000000000000
+}
+
+//π関数の中の1ラウンドの転置
+func permutation(m int) int {
+	var vIn []int
+	vIn = append(vIn, (m >> 24 & 0xff))
+	vIn = append(vIn, (m >> 16 & 0xff))
+	vIn = append(vIn, (m >> 8 & 0xff))
+	vIn = append(vIn, (m & 0xff))
 
 	//v3
 	v3_1 := RotateL8(vIn[3], 2)
@@ -88,38 +81,38 @@ func permutation(vIn []int) []int {
 	v3_3 := RotateL8(v3_2, 1)
 	v3_4 := v0_3 ^ v3_3
 
-	vOut = append(vOut, v0_3)
-	vOut = append(vOut, v1_4)
-	vOut = append(vOut, v2_3)
-	vOut = append(vOut, v3_4)
-
-	return vOut
+	return joinBit(joinBit(joinBit(v0_3, v1_4), v2_3), v3_4)
 }
 
+//算術和ではみ出す桁を除去
 func modPlus(a int, b int) int {
 	return (a + b) % 256
 }
 
+//RotateL8 8bitの数値を対象とした左シフト
 func RotateL8(a int, i int) int {
 	return ((a<<i)&0xff ^ (a >> (8 - i)))
 }
 
+//RotateL32 32bitの数値を対象とした左シフト
 func RotateL32(a int, i int) int {
 	return ((a<<i)&0xffffffff ^ (a >> (32 - i)))
 }
 
+//副鍵を生成
 func createK1(k int) int {
 	if ((k >> 31) & 1) == 0 {
 		return RotateL32(k, 1)
-	} else {
-		return k ^ 0b00000000000000000000000010000111
 	}
+	return k ^ 0b00000000000000000000000010000111
 }
 
-func createK2(k1 int) int {
-	return createK1(createK1(k1))
+//8bitを結合 a->何bitでもよい b->8bit
+func joinBit(a int, b int) int {
+	return (a << 8) | b
 }
 
+//以下2つが任意の範囲で乱数を生成して配列に格納する関数
 func k(m map[int]bool) []int {
 	i := 0
 	result := make([]int, len(m))
@@ -143,7 +136,6 @@ func random(min int, max int, num int) []int {
 		}
 	}
 	keys := k(selected)
-	// ソートしたくない場合は以下1行をコメントアウト
-	sort.Sort(sort.IntSlice(keys))
+	//sort.Sort(sort.IntSlice(keys)) // ソートしない場合コメントアウト
 	return keys
 }
